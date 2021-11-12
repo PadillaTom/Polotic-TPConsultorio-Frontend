403e4b064be3ec9bac5c31cf414d2aed669fa852
Tomas Padilla <padillatomasagustin@gmail.com>
2021-11-12 13:15:28 +0100

    Sidebar Links, Styles, falta Icons y Functionality

--- src/Components/Navigation/Sidebar.js
+++ src/Components/Navigation/Sidebar.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { FaTimes } from "react-icons/fa";
 import {
   patientLinks,
   dentistLinks,
@@ -21,13 +21,19 @@ const Sidebar = () => {
     <div className={`${isSidebarOpen ? "sidebar show-sidebar" : "sidebar"}`}>
       <div className="sidebar-top">
         <div className="sb-top-logo-close">
-          <div className="sb-top-logo">Logo</div>
-          <h2 onClick={closeSidebar} className="sb-top-close">
-            X
-          </h2>
+          <div className="sb-top-logo">Consultorio</div>
+          <FaTimes onClick={closeSidebar} className="sb-top-close"></FaTimes>
         </div>
-        <div className="sb-top-account">Account</div>
+
+        <div className="sb-top-account">
+          <div className="acc-img">{imageUrl}</div>
+          <div className="acc-info">
+            <h3>{username}</h3>
+            <p>{email}</p>
+          </div>
+        </div>
       </div>
+
       <div className="sidebar-links">
         <SideLink id={1} title="Dashboard" url="/" icon="icon"></SideLink>
         <SideCategory category="Patient" icon="Icon"></SideCategory>
