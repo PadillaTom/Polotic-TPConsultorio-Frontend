e18aa52ffe64fdfcee82d8d8d6f1e6bc22d99e73
Tomas Padilla <padillatomasagustin@gmail.com>
2021-11-12 11:17:43 +0100

    Comenzando Sidebar

--- src/Components/Navigation/Sidebar.js
+++ src/Components/Navigation/Sidebar.js
@@ -1,17 +1,28 @@
 import React from "react";
 
+import { links, mockUser } from "../../Utils/constants";
+
 import { useEventsContext } from "../../Context/EventsContext";
 
 const Sidebar = () => {
   const { isSidebarOpen, closeSidebar } = useEventsContext();
 
+  const accountLink = links[0];
+  // MOCK DATA:
+  const { id, username, email, imageUrl } = mockUser;
+
   return (
     <div className={`${isSidebarOpen ? "sidebar show-sidebar" : "sidebar"}`}>
-      992 + Sidebar
-      <br />
-      <br />
-      <br />
-      <h2 onClick={closeSidebar}>close</h2>
+      <div className="sidebar-top">
+        <div className="sb-top-logo-close">
+          <div className="sb-top-logo">Logo</div>
+          <h2 onClick={closeSidebar} className="sb-top-close">
+            X
+          </h2>
+        </div>
+        <div className="sb-top-account">Account</div>
+      </div>
+      <div className="sidebar-links"></div>
     </div>
   );
 };
---
+++ src/Utils/constants.js
@@ -0,0 +1,69 @@
+import { RiAccountCircleFill } from "react-icons/ri";
+import {} from "react-icons";
+import {} from "react-icons";
+import {} from "react-icons";
+
+export const links = [
+  {
+    id: 1,
+    title: "Account",
+    url: "/account",
+  },
+  {
+    id: 2,
+    title: "Dashboard",
+    url: "/",
+  },
+  {
+    id: 3,
+    title: "New Patient",
+    url: "/patient-create",
+  },
+  {
+    id: 4,
+    title: "Patients List",
+    url: "/patient-list",
+  },
+  {
+    id: 5,
+    title: "Patient's Chart",
+    url: "/patient-chart",
+  },
+  {
+    id: 6,
+    title: "New Dentist",
+    url: "/dentist-create",
+  },
+  {
+    id: 7,
+    title: "Dentists List",
+    url: "/dentist-list",
+  },
+  {
+    id: 8,
+    title: "Dentist's Chart",
+    url: "/dentist-chart",
+  },
+  {
+    id: 9,
+    title: "Patients Per Day",
+    url: "/data-patients-day",
+  },
+  {
+    id: 10,
+    title: "Dentist RDV",
+    url: "/data-dentists-rdv",
+  },
+  {
+    id: 11,
+    title: "Insurance Statistics",
+    url: "/patients-insurance",
+  },
+];
+
+export const mockUser = {
+  id: 1,
+  username: "Username",
+  email: "email@email.com",
+  imageUrl: <RiAccountCircleFill></RiAccountCircleFill>,
+};
