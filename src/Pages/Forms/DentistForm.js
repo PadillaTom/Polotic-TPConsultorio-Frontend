81950e9c54cac3e41a39900fe351211ae587b826
Tomas Padilla <padillatomasagustin@gmail.com>
2021-11-17 20:07:13 +0100

    axios Post Dentist and Patients OK

--- src/Pages/Forms/DentistForm.js
+++ src/Pages/Forms/DentistForm.js
@@ -1,16 +1,59 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useHistory } from "react-router-dom";
 
 import { PersonFields } from "../Forms";
 import { BtnSubmit } from "../../Components/Buttons";
 
 const DentistForm = () => {
+  const history = useHistory();
+  const [speciality, setSpeciality] = useState("");
+  const [personData, setPersonData] = useState({});
+  const [postData, setPostData] = useState({});
+
+  const collectPersonFields = (myData) => {
+    setPersonData(myData);
+  };
+
+  useEffect(() => {
+    const finalPost = { ...personData, speciality };
+    setPostData(finalPost);
+  }, [speciality, personData]);
+
+  const handleSubmit = () => {
+    axios
+      .post("http://localhost:8080/dentists/create", postData)
+      .then((res) => {
+        console.log(res.data, res.status); // *** BORRAR EL LOG ***
+        if (res.status === 201) {
+          // SET RECENTLY CREATED ON CONTEXT
+          history.push("/dentist-list");
+          // SHOW MODAL DISPLAYING RECENT CREATION
+        }
+      })
+      .catch((err) => console.log(err));
+  };
+
   return (
     <div className="form-create-container">
-      <form className="form-create">
-        <PersonFields></PersonFields>
+      <form
+        className="form-create"
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleSubmit();
+        }}
+      >
+        <PersonFields collect={collectPersonFields}></PersonFields>
         <div className="fc-single-input">
           <label htmlFor="speciality">Speciality</label>
-          <input type="text" name="speciality" />
+          <input
+            type="text"
+            name="speciality"
+            value={speciality}
+            onChange={(e) => {
+              setSpeciality(e.target.value);
+            }}
+          />
         </div>
         <div className="btn-container">
           <BtnSubmit></BtnSubmit>
